4e0e49d7125cfda564ea5fcd523947562386e516
huyhoanlee <hoanle2003@gmail.com>
2025-02-28 23:19:34 +0700

    delete main branch

--- app/config/__init__.py
+++
@@ -1,9 +0,0 @@
-# Application configuration initialization
-
-class Config:
-    PALATE_WEIGHT_PATH = "./demo/data/models/license_plate_detector.pt"
-    DETECT_WEIGHT_PATH = "./demo/data/models/best.pt"
-    DETECT_CONF = 0.25
-    # source_video_path = "./demo/image.png"
-    source_video_path = "./demo/data/vehicle-counting.mp4"
-    
