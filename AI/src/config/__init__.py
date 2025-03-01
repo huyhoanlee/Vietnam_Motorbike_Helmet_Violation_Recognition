d6d60359ff0bf9e7ca140a34441205525753043f
huyhoanlee <hoanle2003@gmail.com>
2025-03-01 10:29:32 +0700

    add readme for BE

--- AI/src/config/__init__.py
+++
@@ -1,7 +0,0 @@
-# Application configuration initialization
-
-class ModelConfig:
-    DETECT_WEIGHT_PATH = "./src/models/weights/best.pt"
-    PALATE_WEIGHT_PATH = "./src/models/weights/license_plate_detector.pt"
-    DETECT_CONF = 0.25
-    source_video_path = "MVI_0334.MOV"
