a614a80dc56d05c7b2dfe7829e804208db3e7d12
huyhoanlee <hoanle2003@gmail.com>
2025-04-08 23:18:23 +0700

    delete BE code in Frontend branch

--- BE/HVDS_BE/vehicles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import VehicleListCreateView, VehicleRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('', VehicleListCreateView.as_view(), name='vehicle_list_create'),
-    path('<int:pk>/', VehicleRetrieveUpdateDestroyView.as_view(), name='vehicle_detail'),
-]
